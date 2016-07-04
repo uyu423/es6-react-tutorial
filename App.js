2c97842f99162cc5c600424b77529a99897cd4d8
yeaz <changwhan@tumblbug.com>
2016-07-05 00:51:28 +0900

    Use function to define App instead of class

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-class App extends React.Component {
-  render() {
-    return <div>Hello</div>
-  }
-}
+
+// class App extends React.Component {
+//   render() {
+//     return <div>Hello</div>
+//   }
+// }
+
+const App = () => <div>Hello Guys</div>
 
 export default App
