b3a59fb1b9c8d53ecd1f7be4632ebb548aa647c9
yeaz <changwhan@tumblbug.com>
2016-07-05 00:54:41 +0900

    Use render method with parentheses

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-// class App extends React.Component {
-//   render() {
-//     return <div>Hello</div>
-//   }
-// }
-
-const App = () => <div>Hello Guys</div>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    );
+  }
+}
 
 export default App
